5e24e4648f68a8297ea98a562ff0be0a60e9f461
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2025-11-17 19:39:00 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -5,7 +5,7 @@ def split_before_each_uppercases(formula):
     split_formula = []
     for i in range(1, len(formula)):
         if formula[i].isupper():
-            split_formula.append(formula{start:1])
+            split_formula.append(formula[start:1])
             start=1
     split_formula.append(formula[start:])
     return split_formula
