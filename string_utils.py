269a52fd0ca800d6a49256f69a1d24bbd63cdeaf
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2025-11-17 19:56:52 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -12,12 +12,18 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    digit_location=1
-    while digit_location<len(formula) and not formula[digit_location].isdigit():
-        digit_location+=1
-        if digit_location==len(formula):
-          return formula,1
-        
-    return formula[:digit_location],int(formula[digit_location:])
+    digit_location=-1
+    for i in range(len(formula)):
+        if fomula[i].isdigit():
+            digit-location=i
+            break
+    if digit_location==-1:
+        return formula,1
+    pref=formula[:digit_location]
+    p=digit_location
+    while p<len(formula) and fomula[p].isdigit():
+        p+=1
+    num = int(formula[digit_location:p])
+    return pref,num
 
 
